3997a817116ebca9f2cc85f67d68d7526a9f21eb
KatherineDuncan <duncan@psych.utoronto.ca>
2021-10-18 18:26:45 -0400

    updated for 2021

--- PS_skeleton.py
+++ PS_skeleton.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-pandorable problem set 3 for PSY 1210 - Fall 2019
+pandorable problem set 3 for PSY 3100 - Fall 2021
 
 @author: katherineduncan
 
@@ -30,7 +30,7 @@ IAT = ...
 # 1 genderidentity->gender
 # 2 raceomb_002->race
 # 3 edu->edu
-# 4 politicalid_7->politic
+# 4 politicalid_7->political
 # 5 STATE -> state
 # 6 att_7->attitude
 # 7 tblacks_0to10-> tblack
@@ -41,13 +41,10 @@ IAT = ...
 
 IAT = ...
 
-# remove all participants that have at least one missing value
-IAT_clean = ...
-
 
 # check out the replace method: https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.replace.html
 # use this to recode gender so that 1=men and 2=women (instead of '[1]' and '[2]')
-IAT_clean ...
+IAT...
 
 # use this cleaned dataframe to answer the following questions
 
@@ -57,13 +54,13 @@ IAT_clean ...
 # use sorting and indexing to print out the following information:
 
 # 2a: the ids of the 5 participants with the fastest reaction times
-
+...
 
 # 2b: the ids of the 5 men with the strongest white-good bias
-
+...
 
 # 2c: the ids of the 5 women in new york with the strongest white-good bias
-
+...
 
 
 #%%
@@ -75,18 +72,16 @@ states =...
 
 # write a loop that iterates over states to calculate the median white-good
 # bias per state
-# store the results in a dataframe with 2 columns: state & bias
+# store the results in a stateBias dataframe with 2 columns: states & bias
 # hint you will need to use indexing to store the median on each iteration
-for ...
+# I recomend using iloc
+sind=0
+stateBias=pd.DataFrame(states,columns=['states'])
+stateBias['bias']=None
 
+for ...
+            
 
 # now use the pivot_table function to calculate the same statistics as the above loop
 state_bias=...
 
-# add a new variable to the IAT dataframe that codes for whether or not a
-# participant identifies as black/African American
-
-
-# make another pivot_table that calculates median bias per state, separately
-# for those that do and do not identify as black/African American
-state_race_bias=...
